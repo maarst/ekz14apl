dd07d6822cd47194f5f4f8f569517cb4a99a46ae
maarst <umpleasant21@gmail.com>
2026-04-13 23:02:30 +0300

    Add files via upload

---
+++ 190126/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _190126
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isFirst = true;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            new SecondWindow().Show();
+        }
+
+        private void ShowMessage_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("привет");
+        }
+
+        private void EnglishTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        { 
+            e.Handled = !Regex.IsMatch(e.Text, @"^[a-zA-Z ]+$");
+        }
+
+        private void ToggleImage_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            isFirst = !isFirst;
+            ToggleImage.Source = new BitmapImage(new Uri(isFirst ? "image1.png" : "image2.png", UriKind.Relative));
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            ((CheckBox)sender).IsEnabled = false;
+        }
+
+    }
+}
---
+++ 190126/SecondWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace _190126
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+            this.Closed += (s, e) => Application.Current.Shutdown();
+        }
+
+        private void ManualDateBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"^[0-9]+$");
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            Random rnd = new Random();
+            string fileName = $"блокнот_{rnd.Next(1, 10000)}.txt";
+            File.WriteAllText(fileName, NoteTextBox.Text);
+        }
+    }
+}
