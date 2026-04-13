d197e15c1112b59dde1c017aeb8c8789bd329035
maarst <umpleasant21@gmail.com>
2026-04-13 22:44:34 +0300

    Add files via upload

---
+++ 22.01.2026/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _22._01._2026
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                Pic1.Source = new BitmapImage(new Uri("img1.png", UriKind.Relative));
+                Pic2.Source = new BitmapImage(new Uri("img2.png", UriKind.Relative));
+            }
+            catch { }
+        }
+
+        private void T2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text.Length > 0)
+                tb.BorderThickness = new Thickness(0, 0, 0, 1);
+            else
+                tb.BorderThickness = new Thickness(0, 0, 0, 0);
+        }
+
+        private void T3_Main_LostFocus(object sender, RoutedEventArgs e)
+        {
+            T3_Main.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void T3_Main_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                T3_Main.TextDecorations = TextDecorations.Underline;
+            }
+        }
+
+        private void EngInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string eng = "QWERTYUIOP[]ASDFGHJKL;'ZXCVBNM,.qwertyuiop[]asdfghjkl;'zxcvbnm,.";
+            string rus = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъфывапролджэячсмитьбю";
+
+            string input = EngInput.Text;
+            string result = "";
+
+            foreach (char c in input)
+            {
+                int index = eng.IndexOf(c);
+                if (index != -1)
+                {
+                    result += rus[index];
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            RusOutput.Text = result;
+        }
+
+        private void Pic1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                var temp = Pic1.Source;
+                Pic1.Source = Pic2.Source;
+                Pic2.Source = temp;
+            }
+        }
+
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (Combo1.SelectedIndex == 1)
+            {
+                Combo2.SelectedIndex = 1; 
+                Combo3.SelectedIndex = 1; 
+            }
+        }
+    }
+}
+
